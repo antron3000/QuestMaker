5840fb65ade196003131424b3f674a94be2eefb9
Antoine De Vuyst <fortyonepointsix@gmail.com>
2018-05-26 18:30:35 -0400

    first commit

---
+++ Contracts/contracts/Quest.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.4.23;
+
+import "./QuestCards.sol";
+import "./MerkleProof.sol";
+
+contract Quest is QuestCards {
+
+    uint256 QuestCount = 0;
+    mapping (uint=>Quest) Quests;
+    uint256[] QuestList;
+
+
+   struct Quest {
+        address Creator;
+        uint256 ID;
+        string name;
+        string hint;
+        bytes32 merkleRoot;
+        string CardName;
+   }
+
+
+
+    function createQuest(string _name, string _hint, uint _numTokens, bytes32 _merkleRoot, string _cardName) public returns (uint256) {
+        Quest memory newQuest;
+        uint256 QuestID = QuestCount++;
+        newQuest.name = _name;
+        newQuest.hint = _hint;
+        newQuest.merkleRoot = _merkleRoot;
+        newQuest.ID = QuestID;
+        newQuest.Creator = msg.sender;
+        newQuest.CardName = _cardName;
+        Quests[QuestID] = newQuest;
+        QuestList.push(QuestID);
+
+        return QuestID;
+        }
+
+    function proveLocation(uint _questID, bytes32[] _proof, bytes32 _leaf) returns (bool){
+
+        if(MerkleProof.verifyProof(_proof,Quests[_questID].merkleRoot,_leaf)){
+            mintTo(msg.sender,"",Quests[_questID].CardName);
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+
+    //return list of Quests
+    function getQuestList() returns (uint256[]){
+        return QuestList;
+    }
+
+    function getQuest(uint256 _ID) returns (string, string, bytes32, address){
+        Quest temp = Quests[_ID];
+        return (temp.name, temp.hint, temp.merkleRoot, temp.Creator);
+    }
+
+}
